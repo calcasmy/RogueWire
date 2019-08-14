6ea396fb90c24521dee6e6967cb49c8f12e990d4
calcasmy <technoatomic@gmail.com>
2019-08-13 22:44:27 -0400

    Added the Rogue file with main()

---
+++ src/main/java/RogueWire/Rogue.java
@@ -0,0 +1,14 @@
+package RogueWire;
+
+public class Rogue {
+
+	public Rogue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
